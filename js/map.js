eaaaff9f64c32842a1d9bd3cb54084477dc8cea0
ZKingDRFN <33327219+ZKingDRFN@users.noreply.github.com>
2018-08-22 12:32:10 -0700

    Create map.js

---
+++ js/map.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoicG9pbnRzbm9ydGgiLCJhIjoiY2l6azZqc2R4MDJ6bzJxb2QwMHFkOGtiaSJ9.5BkL6zVbekl1hCbDy1vbCA';
+var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
+    center: [-74.50, 40], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
