f6959d81d82b02713230ddfe54f77a60c39e16bf
ZKingDRFN <33327219+ZKingDRFN@users.noreply.github.com>
2018-08-22 12:59:44 -0700

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,8 +1,8 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoicG9pbnRzbm9ydGgiLCJhIjoiY2l6azZqc2R4MDJ6bzJxb2QwMHFkOGtiaSJ9.5BkL6zVbekl1hCbDy1vbCA';
 var map = new mapboxgl.Map({
     container: 'map', // container id
-    style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
-    center: [-74.50, 40], // starting position [lng, lat]
+    style: 'mapbox://styles/marenab/cjl2oczit56x42rlxm5adlopg ', // stylesheet location
+    center: [-54, 40], // starting position [lng, lat]
     zoom: 9 // starting zoom
 });
 // Add zoom and rotation controls to the map.
